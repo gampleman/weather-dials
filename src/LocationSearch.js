82edd339b065bef648b135d763c14fb7bc9d865a
Jakub Hampl <kopomir@gmail.com>
2019-09-13 11:46:39 +0200

    Implements more of location search

--- src/LocationSearch.js
+++ src/LocationSearch.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+let currentId = 0;
 
-const LocationSearch = props => {
-  const autocomplete = () => null;
+const useId = () => {
+  const ref = useRef(0);
+  if (ref.current === 0) {
+    ref.current = ++currentId;
+  }
+  return "id-" + ref.current;
+};
+
+const LocationSearch = ({ onRender }) => {
+  const inputId = useId();
+  const [value, setValue] = useState("");
   return (
     <div>
-      <label>
-        City:
-        <input type="text" onChange={autocomplete} />
-      </label>
+      <label htmlFor={inputId}>City:</label>
+      <input
+        id={inputId}
+        autoFocus={true}
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button onClick={() => onRender(value)}>Render</button>
     </div>
   );
 };
--- src/LocationSearch.test.js
+++ src/LocationSearch.test.js
@@ -1,10 +1,21 @@
 import React from "react";
 import LocationSearch from "./LocationSearch";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("LocationSearch", () => {
   it("shows a search bar for the location", () => {
     const { getByLabelText } = render(<LocationSearch />);
     expect(getByLabelText("City:")).toBeInTheDocument();
+  });
+
+  it("allows you to start rendering", () => {
+    const onRender = jest.fn();
+    const { getByText, getByLabelText } = render(
+      <LocationSearch onRender={onRender} />
+    );
+    const input = getByLabelText("City:");
+    fireEvent.input(input, { target: { value: "Edinburgh" } });
+    fireEvent.click(getByText("Render"));
+    expect(onRender).toHaveBeenCalledWith("Edinburgh");
   });
 });
